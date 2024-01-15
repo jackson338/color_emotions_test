6eaa72a49f92541177545e607722d74d55002a61
jackson338 <jacksondoaks@gmail.com>
2024-01-15 13:37:17 -0700

    The bug was a range error. I now dynamically find the range in the list based on it's length which prevents the range error from occuring.

--- health_app/lib/color_bloc/color_bloc.dart
+++ health_app/lib/color_bloc/color_bloc.dart
@@ -22,64 +22,29 @@ class ColorCubit extends Cubit<ColorState> {
   }
 
   void addNewColor(
-    // String start,
-    // String end,
     int total,
     String colorName,
     Color color,
   ) {
     List<ColorData> colors = [...state.colorData];
-    final newColor = ColorData(
-        // start: start,
-        // end: end,
-        total: total,
-        color: color,
-        colorName: colorName);
+    final newColor = ColorData(total: total, color: color, colorName: colorName);
     colors.add(newColor);
     emit(state.copyWith(colorData: colors));
   }
 
-  // void aggregateAndSortColors() {
-  //   // Step 1: Aggregate durations
-  //   Map<Color, int> aggregatedDurations = {};
-  //   for (final data in state.colorData) {
-  //     if (data.total.isNotEmpty) {
-  //       int duration = int.parse(data.total);
-  //       if (aggregatedDurations.containsKey(data.color)) {
-  //         aggregatedDurations[data.color] = aggregatedDurations[data.color]! + duration;
-  //       } else {
-  //         aggregatedDurations[data.color] = duration;
-  //       }
-  //     }
-  //   }
-
-  //   // Step 2: Convert map to list
-  //   List<ColorData> aggregatedList = aggregatedDurations.entries.map((entry) {
-  //     return ColorData(
-  //       total: '${entry.value}',
-  //       colorName: colorsMap[entry.key]!, // You may need to handle colorName differently
-  //       color: entry.key,
-  //     );
-  //   }).toList();
-
-  //   // Step 3: Sort the list
-  //   aggregatedList.sort((a, b) => int.parse(b.total).compareTo(int.parse(a.total)));
-  //   emit(state.copyWith(aggregatedData: aggregatedList));
-  // }
-
   void assessEmotions() {
-    int sad = 0;
-    int happy = 0;
-    int stress = 0;
-    int relaxed = 0;
-    int anxious = 0;
-    int calm = 0;
-    int tired = 0;
-    int energized = 0;
-    int insecure = 0;
-    int confident = 0;
-    int pessimistic = 0;
-    int optimistic = 0;
+    int sad = 1;
+    int happy = 1;
+    int stress = 1;
+    int relaxed = 1;
+    int anxious = 1;
+    int calm = 1;
+    int tired = 1;
+    int energized = 1;
+    int insecure = 1;
+    int confident = 1;
+    int pessimistic = 1;
+    int optimistic = 1;
     Set<String> totalColors = {};
     for (final cd in state.colorData) {
       totalColors.add(cd.colorName);
@@ -120,27 +85,6 @@ class ColorCubit extends Cubit<ColorState> {
       if (cd.colorName == 'light green') {
         optimistic += duration;
       }
-      // if (duration < 30) {
-      //   anxious += 40;
-      // }
-      // if (duration < 55 && duration > 30) {
-      //   stress += 40;
-      // }
-      // if (duration < 100 && duration > 55) {
-      //   happy += 40;
-      // }
-      // if (duration > 150 && duration < 250) {
-      //   calm += 40;
-      // }
-      // if (duration > 300 && duration < 400) {
-      //   sad += 40;
-      // }
-      // if (duration > 400 && duration < 500) {
-      //   relaxed += duration;
-      // }
-      // if (duration > 500) {
-      //   relaxed += 40;
-      // }
     }
     sad += sadMovements(state);
     happy += happyMovements(state);
